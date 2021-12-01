2ccdde9c5444dbe81b2d26630d02ee1fa92c7809
artidata <imaduddin.htm@gmail.com>
2021-12-01 20:15:58 +0000

    Removing unnecessary console log.

--- 2021/1/2.ts
+++ 2021/1/2.ts
@@ -1,7 +1,7 @@
 import fs = require('fs')
 let arr = fs.readFileSync('./1.txt', 'utf8').split('\n')
 let count = 0
-for (let i = 1; i < arr.length; i++) {
-  count += parseInt(arr[i]) > parseInt(arr[i - 1]) ? 1 : 0
+for (let i = 0; i < arr.length - 2; i++) {
+  count += parseInt(arr[i]) < parseInt(arr[i + 3]) ? 1 : 0
 }
 console.log(count)
