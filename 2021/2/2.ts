044b50693b0a572ccfd728838b3c9e72fa01e3d4
artidata <imaduddin.htm@gmail.com>
2021-12-02 17:21:43 +0000

    Solving 2nd part of the puzzle.

---
+++ 2021/2/2.ts
@@ -0,0 +1,29 @@
+import fs = require('fs')
+
+let arr = fs.readFileSync('./1.txt', 'utf8').split('\n')
+
+let a = 0
+let h = 0
+let v = 0
+
+arr.forEach((val) => {
+  let ins: string
+  let num: string | number
+  ;[ins, num] = val.split(' ')
+  num = parseInt(num)
+
+  switch (ins) {
+    case 'forward':
+      h += num
+      v += a * num
+      break
+    case 'down':
+      a += num
+      break
+    case 'up':
+      a -= num
+      break
+  }
+})
+
+console.log(h * v)
