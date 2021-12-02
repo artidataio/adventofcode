4682fde92313e5185beceba48849c130543e5d95
artidata <imaduddin.htm@gmail.com>
2021-12-02 16:07:19 +0000

    Type inference may cause array destructuring more verbose.

---
+++ 2021/2/1.ts
@@ -0,0 +1,27 @@
+import fs = require('fs')
+
+let arr = fs.readFileSync('./1.txt', 'utf8').split('\n')
+
+let h = 0
+let v = 0
+
+arr.forEach((val) => {
+  let ins: string
+  let num: string | number
+  ;[ins, num] = val.split(' ')
+  num = parseInt(num)
+
+  switch (ins) {
+    case 'forward':
+      h += num
+      break
+    case 'down':
+      v += num
+      break
+    case 'up':
+      v -= num
+      break
+  }
+})
+
+console.log(h * v)
