52ca67ece56a1f91b750f57137ea5aac6cde9e8e
artidata <imaduddin.htm@gmail.com>
2021-12-02 12:20:14 +0000

    parseInt init in map

--- 2021/1/2.ts
+++ 2021/1/2.ts
@@ -1,7 +1,14 @@
 import fs = require('fs')
-let arr = fs.readFileSync('./1.txt', 'utf8').split('\n')
+
+let arr = fs
+  .readFileSync('./1.txt', 'utf8')
+  .split('\n')
+  .map((val) => parseInt(val))
+
 let count = 0
+
 for (let i = 0; i < arr.length - 2; i++) {
-  count += parseInt(arr[i]) < parseInt(arr[i + 3]) ? 1 : 0
+  count += arr[i] < arr[i + 3] ? 1 : 0
 }
+
 console.log(count)
