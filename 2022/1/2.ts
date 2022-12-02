eadc59f99d378a7023ec8f4bd7cc228fe379024a
Imaduddin Haetami <imaduddin.htm@gmail.com>
2022-12-02 19:34:04 +0700

    Add solution to day 1 part 2.

---
+++ 2022/1/2.ts
@@ -0,0 +1,29 @@
+import { readFileSync } from "fs";
+let arr = readFileSync("1/1.txt", "utf8").split(/\r?\n/);
+
+let current = 0;
+let top3 = [0, 0, 0];
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i].length === 0) {
+    if (current > top3[0]) {
+      top3 = [current, top3[0], top3[1]];
+    } else if (current > top3[1]) {
+      top3 = [top3[0], current, top3[1]];
+    } else if (current > top3[2]) {
+      top3 = [top3[0], top3[1], current];
+    }
+    current = 0;
+  } else {
+    current += parseInt(arr[i]);
+  }
+}
+//corner case
+if (current > top3[0]) {
+  top3 = [current, top3[1], top3[2]];
+} else if (current > top3[1]) {
+  top3 = [top3[0], current, top3[1]];
+} else if (current > top3[2]) {
+  top3 = [top3[0], top3[1], current];
+}
+
+console.log(top3[0] + top3[1] + top3[2]);
