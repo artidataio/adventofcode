3cf71b29f6f931245fdbd05dd868a038f6ad5252
Imaduddin Haetami <imaduddin.htm@gmail.com>
2022-12-02 19:18:00 +0700

    Add solution to the day 1 problem 1

--- 2022/1/1.ts
+++ 2022/1/1.ts
@@ -1,3 +1,21 @@
 import { readFileSync } from "fs";
 let arr = readFileSync("1/1.txt", "utf8").split(/\r?\n/);
-console.log(arr);
+
+let current = 0;
+let max = 0;
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i].length === 0) {
+    if (current > max) {
+      max = current;
+    }
+    current = 0;
+  } else {
+    current += parseInt(arr[i]);
+  }
+}
+//corner case
+if (current > max) {
+  max = current;
+}
+
+console.log(max);
