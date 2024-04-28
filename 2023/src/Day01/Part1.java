041cfdeb608b2c9ead75bb71c01180c7d3c9efea
Imaduddin Haetami <imaduddin.htm@gmail.com>
2024-04-28 10:46:26 +0700

    Prefer procedural rather than splitting them into functions.

--- 2023/src/Day01/Part1.java
+++ 2023/src/Day01/Part1.java
@@ -7,33 +7,19 @@ import utils.ImportUtils;
 
 public class Part1 {
 
-    static String removeLetters(String s) {
-        return s.replaceAll("[a-z]", "");
-    }
-
-    static String enforce2Digits(String s) {
-        if (s.length() > 2) {
-            return String.valueOf(s.charAt(0)) +
-                    s.charAt(s.length() - 1);
-        }
-        if (s.length() < 2) {
-            return s + s;
-        } else {
-            return s;
-        }
-    }
-
     public static void main(String[] args) {
 
         String filePath = Path.of("asset/day-01/input.txt").toString();
         List<String> input = ImportUtils.readAsList(filePath);
-        int out = input
-                .stream()
-                .map(item -> Integer.parseInt(enforce2Digits(removeLetters(item))))
-                .mapToInt(Integer::intValue)
-                .sum();
 
-        System.out.println(out);
+        int count = 0;
+
+        for (String elem : input) {
+            String digits = elem.replaceAll("[a-z]", "");
+            count += Integer.parseInt("" + digits.charAt(0) + digits.charAt(digits.length() - 1));
+        }
+
+        System.out.println(count);
 
     }
 }
